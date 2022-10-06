a1c27334cfb40f6a576ef78554ba73ed3e3c11ef
Jeff <Examene101@gmail.com>
2022-10-06 16:14:05 -0400

    completed  Objective2Lab3

---
+++ labs/Objective2Lab3.java
@@ -0,0 +1,15 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+
+    /*
+      Your solution goes here
+    */
+    int yearBorn = 1989;
+    String monthBorn = "August";
+    boolean weekendTrueOrFalse = false;
+
+    System.out.println(yearBorn + "\n" + monthBorn + "\n" + weekendTrueOrFalse);
+
+  }
+}

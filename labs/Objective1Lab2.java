bff0262deb6fa7e42222e669f35a80efcc224a47
Jeff <Examene101@gmail.com>
2022-10-06 09:35:15 -0400

    complete objective1lab2

---
+++ labs/Objective1Lab2.java
@@ -0,0 +1,18 @@
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+    /*
+      Your solution goes here
+    */
+    String pipe = "|";
+    String arrows = ">>";
+    String front = arrows + " " + pipe;
+
+    System.out.println(arrows + " "+ "________________");
+    System.out.println(front + " " + "Grocery List" + " " + pipe);
+    System.out.println(front + " " + " 1. Milk " + " " + pipe);
+    System.out.println(front + " " + " 2. Eggs " + " " + pipe);
+    System.out.println(front + " " + " 3. Bread " + " " + pipe);
+    System.out.println(arrows + " "+ "________________");
+
+  }
+}
